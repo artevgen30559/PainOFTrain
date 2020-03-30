26aef044f18663867a1d0033a1c6402982829501
Artevgen <artevgen.work@gmail.com>
2020-03-30 20:52:15 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+	//include('pdo.php');
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Pain of Train</title>
+	<link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+</body>
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
+<script src="index.js"></script>
+
+</html>
